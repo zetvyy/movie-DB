eaee4557f5eec62855de22505c03e860cf3908d2
zetvyy <zetvyy@gmail.com>
2021-09-15 11:37:07 +0700

    menghapus codingan yg lama

--- script.js
+++ script.js
@@ -1,33 +1,3 @@
-// const searchButton = document.querySelector(".search-button");
-// searchButton.addEventListener("click", function() {
-//   //
-//   const inputKeyword = document.querySelector(".input-keyword");
-//   fetch("http://www.omdbapi.com/?apikey=91845964&s=" + inputKeyword.value)
-//     .then(response => response.json())
-//     .then(response => {
-//       const movies = response.Search;
-//       let cards = "";
-//       movies.forEach(m => (cards += showCards(m)));
-//       const movieContainer = document.querySelector(".movie-container");
-//       movieContainer.innerHTML = cards;
-
-//       // ketika tombol detail diklik
-//       const modalDetailButton = document.querySelectorAll(".modal-detail-button");
-//       modalDetailButton.forEach(btn => {
-//         btn.addEventListener("click", function() {
-//           const imdbid = this.dataset.imdbid;
-//           fetch("http://www.omdbapi.com/?apikey=91845964&i=" + imdbid)
-//             .then(response => response.json())
-//             .then(m => {
-//               const movieDetail = showMovieDetail(m);
-//               const modalBody = document.querySelector(".modal-body");
-//               modalBody.innerHTML = movieDetail;
-//             });
-//         });
-//       });
-//     });
-// });
-
 const searchButton = document.querySelector(".search-button");
 searchButton.addEventListener("click", async function() {
   const inputKeyword = document.querySelector(".input-keyword");
